79584b6529ae142ba58389e0210fbb804fc609ec
Analaura_10 <ana.garcia4@estudante.ifms.edu.br>
2023-11-25 11:35:35 -0400

    first commit

---
+++ componentes/Rodape.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from '../src/styles/Components.module.css';
+export default function Rodape() {
+    return (
+    <div className={styles.rodape}> 
+        <p>
+            ANA LAURA DIAS, JUNIELI MEDEIROS, EDILAINE ROCHA
+        </p>
+        <p>
+            FRAMEWORKS2 - RICARDO NASCIMENTO
+        </p>
+        
+    </div>
+    )
+}
